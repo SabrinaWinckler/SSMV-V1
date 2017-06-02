286e0ddddb34c242f050763acbefa4f15c758a47
Gustavo Satheler <gustavosatheler@gmail.com>
2017-06-02 18:52:37 -0300

    v0.16
    Estrutura PJ concluida

--- pj/perfil.php
+++ pj/perfil.php
@@ -3,7 +3,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Bem-vindo</h1>
+                    <h1 class="page-header">Perfil</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
