f30f8bec9ccb325ccf281aff1f5c0713582e90f6
Rodrigo Oliveira <roliveira.loki@gmail.com>
2017-06-18 17:51:59 -0300

    Mudanças CSS
    Codigo reorganizado, colocada transition (ainda precisa mexer)

--- painel/index.php
+++ painel/index.php
@@ -7,71 +7,124 @@ require_once "inc/header.php";
 ?>
         <div id="page-wrapper">
             <div class="row">
+
                 <div class="col-lg-12">
                     <h1 class="page-header">Requisitos</h1>
                 </div>
+
             </div>
+
                 <div class="row">
+
                     <div class="listareq">
                         <div class="col-sm-2 boxReq">
                             <p class="aparecer1">Requisição 1</p>
                             <p class="aparecer2"> Dados da Requisição </p>
                         </div>
                     </div>
-                        <div class=col-sm-1></div>
+
+                    <div class=col-sm-1></div>
+
                     <div class="listareq">
                         <div class="col-sm-2 boxReq">
                             <p class="aparecer1">Requisição 2</p>
                             <p class="aparecer2"> Dados da Requisição </p>
                         </div>
                     </div>
-                        <div class=col-sm-1></div>
+
+                    <div class=col-sm-1></div>
+
                     <div class="listareq">
                         <div class="col-sm-2 boxReq">
                             <p class="aparecer1">Requisição 2</p>
                             <p class="aparecer2"> Dados da Requisição </p>
                         </div>
-                        <div class=col-sm-1></div>
+                    </div>
+
+                    <div class=col-sm-1></div>
+
                     <div class="listareq">
                         <div class="col-sm-2 boxReq">
                             <p class="aparecer1">Requisição 4</p>
                             <p class="aparecer2"> Dados da Requisição </p>
                         </div>
                     </div>
+
+               </div>
+
+                <div class="row">
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 5</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
+                    </div>
+
+                    <div class=col-sm-1></div>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 6</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
+                    </div>
+
+                    <div class=col-sm-1></div>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 7</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
+                    </div>
+
+                    <div class=col-sm-1></div>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 8</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
+                    </div>
+
                </div>
                 <div class="row">
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 5</p>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 9</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
                     </div>
+
                     <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 6</p>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 10</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
                     </div>
+
                     <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 7</p>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 11</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
                     </div>
+
                     <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 8</p>
+
+                    <div class="listareq">
+                        <div class="col-sm-2 boxReq">
+                            <p class="aparecer1">Requisição 12</p>
+                            <p class="aparecer2"> Dados da Requisição </p>
+                        </div>
                     </div>
-               </div>
-                <div class="row">
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 9</p>
-                    </div>
-                    <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 10</p>
-                    </div>
-                    <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 11</p>
-                    </div>
-                    <div class=col-sm-1></div>
-                    <div class="col-sm-2 boxReq">
-                    <p>Requisição 12</p>
-                    </div>
+
                </div>
               
         </div>
